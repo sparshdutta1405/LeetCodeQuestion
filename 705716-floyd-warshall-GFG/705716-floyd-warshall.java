0707139c6a8c8c952400ef55fbed61b46df61392
SPARSHDUTTA <195340215+sparshdutta1405@users.noreply.github.com>
2026-05-22 23:31:44 +0530

    Added solution - LeetHub

---
+++ 705716-floyd-warshall-GFG/705716-floyd-warshall.java
@@ -0,0 +1,23 @@
+// User function template for JAVA
+
+class Solution {
+    public void floydWarshall(int[][] dist) {
+        // Code here
+        int n = dist.length;
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (dist[i][k] != 1e8 && dist[k][j] != 1e8) {
+                        if (dist[i][k] + dist[k][j] < dist[i][j]) {
+                            dist[i][j] = dist[i][k] + dist[k][j];
+                        }
+                    }
+                }
+            }
+        }    
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
