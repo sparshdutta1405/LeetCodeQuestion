02ed9d8963236be5eca0082b271e23dd68bba4cd
SPARSHDUTTA <195340215+sparshdutta1405@users.noreply.github.com>
2026-08-20 10:23:40 +0530

    Reverse Linkedlist GFG using recursion

---
+++ 700005-reverse-a-linked-list-GFG/700005-reverse-a-linked-list.java
@@ -0,0 +1,31 @@
+/* Structure of Linked List Node
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+*/
+
+class Solution {
+    Node reverseList(Node head) {
+        // code here
+        if(head == null || head.next == null){
+            return head;
+        }
+
+        Node temp = reverseList(head.next);
+
+        head.next.next = head;
+        head.next = null;
+        return temp;
+        
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
