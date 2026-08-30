f959e9a3db19b9784c32629ed000cec3e499eaaf
SPARSHDUTTA <195340215+sparshdutta1405@users.noreply.github.com>
2026-08-30 19:37:08 +0530

    Time: 3 ms (61.82%), Space: 86.7 MB (40%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 3090-removing-minimum-and-maximum-from-array/3090-removing-minimum-and-maximum-from-array.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int minimumDeletions(int[] nums) {
+        int n = nums.length;
+        if (n <= 2) return n;
+
+        int minIdx = 0;
+        int maxIdx = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (nums[i] < nums[minIdx]) {
+                minIdx = i;
+            }
+            if (nums[i] > nums[maxIdx]) {
+                maxIdx = i;
+            }
+        }
+
+        int left = Math.min(minIdx, maxIdx);
+        int right = Math.max(minIdx, maxIdx);
+
+        int frontOnly = right + 1;
+        int backOnly = n - left;
+
+        int bothEnds = (left + 1) + (n - right);
+
+        return Math.min(frontOnly, Math.min(backOnly, bothEnds));
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
