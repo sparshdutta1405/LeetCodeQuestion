88ccece577f6ab5e9e91f70d1664b21e25439e34
SPARSHDUTTA <195340215+sparshdutta1405@users.noreply.github.com>
2026-09-01 12:45:44 +0530

    Time: 4 ms (100%), Space: 46.6 MB (51.59%) - LeetHub - Time: O(1), Space: O(k)
    Design a Circular Queue

---
+++ 0622-design-circular-queue/0622-design-circular-queue.java
@@ -0,0 +1,77 @@
+class MyCircularQueue {
+
+    private final int[] queue;
+    private int head;
+    private int count;
+    private final int capacity;
+
+    public MyCircularQueue(int k) {
+        this.capacity = k;
+        this.queue = new int[k];
+        this.head = 0;
+        this.count = 0;
+        
+    }
+    
+    public boolean enQueue(int value) {
+        if(isFull())
+        return false;
+
+        int tail = (head + count) % capacity;
+        queue[tail] = value;
+        count++;
+        return true;        
+    }
+    
+    public boolean deQueue() {
+        if(isEmpty())
+        return false;
+
+        head = (head+1)%capacity;
+        count--;
+        return true;
+        
+    }
+    
+    public int Front() {
+        if(isEmpty())
+        return-1;
+
+        return queue[head];
+        
+    }
+    
+    public int Rear() {
+        if(isEmpty())
+        return -1;
+
+        int tail = (head + count - 1) % capacity;
+        return queue[tail];
+    }
+    
+    public boolean isEmpty() {
+
+        return count == 0;
+        
+    }
+    
+    public boolean isFull() {
+        return count == capacity;
+        
+    }
+}
+
+/**
+ * Your MyCircularQueue object will be instantiated and called as such:
+ * MyCircularQueue obj = new MyCircularQueue(k);
+ * boolean param_1 = obj.enQueue(value);
+ * boolean param_2 = obj.deQueue();
+ * int param_3 = obj.Front();
+ * int param_4 = obj.Rear();
+ * boolean param_5 = obj.isEmpty();
+ * boolean param_6 = obj.isFull();
+ */
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
