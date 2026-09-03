8e069e1dd015120563b603ad30cb727a9913860d
SPARSHDUTTA <195340215+sparshdutta1405@users.noreply.github.com>
2026-09-03 20:56:49 +0530

    Time: 1 ms (16.19%), Space: 45.6 MB (7.21%) - LeetHub - Time: O(N), Space: O(N)
    Validate Binary Search tree 98

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return validate(root, null, null);
+    }
+
+    private boolean validate(TreeNode node, Integer low, Integer high) {
+        if (node == null) return true;
+
+        if ((low != null && node.val <= low) || (high != null && node.val >= high)) {
+            return false;
+        }
+
+        return validate(node.left, low, node.val) && validate(node.right, node.val, high);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
