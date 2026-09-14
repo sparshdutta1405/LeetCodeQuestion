71da8cdecf83d8f253b04e889da2f5881d2ee18f
SPARSHDUTTA <195340215+sparshdutta1405@users.noreply.github.com>
2026-09-14 20:55:16 +0530

    Time: 0 ms (100%), Space: 44.7 MB (87.81%) - LeetHub - Time: O(n), Space: O(1)
    Minimum Cost Climbing Stairs - LeetCode

---
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int prev2 = 0;
+        int prev1 = 0;
+
+        for(int i = 2; i <= cost.length; i++){
+            int curr = Math.min(prev1 + cost[i - 1], prev2 + cost[i - 2]);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return prev1;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
