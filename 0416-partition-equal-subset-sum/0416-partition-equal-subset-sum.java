46219a219681b89ddf94d9b13622fa7be4e9473e
SPARSHDUTTA <195340215+sparshdutta1405@users.noreply.github.com>
2026-09-15 19:42:13 +0530

    Time: 62 ms (40.12%), Space: 47.5 MB (59.94%) - LeetHub - Time: O(n * sum), Space: O(n * sum)
    Partition Equal Subset Sum - LeetCode {Tabulation}

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,44 +1,34 @@
 class Solution {
     public boolean canPartition(int[] nums) {
-        
         int sum = 0;
-        
-        for(int n: nums){
-            sum += n;
+
+        for(int num : nums){
+            sum += num;
         }
 
-        if(sum %2 != 0){
+        if(sum % 2 != 0){
             return false;
         }
 
         int target = sum / 2;
-        Boolean[][] dp = new Boolean[nums.length][target +1];
+        int n = nums.length;
 
-        return solve(nums, target, dp, 0);
-    }
+        boolean[][] dp = new boolean[n+1][target +1];
 
-    private boolean solve(int[] nums, int target, Boolean[][] dp, int index){
-        if(target == 0){
-            return true;
+        dp[0][0] = true;
+
+        for(int i = 1; i <= n ; i++){
+            int num = nums[i-1];
+            for(int j = 0; j <= target; j++){
+                dp[i][j] = dp [i-1][j];
+
+                if(num <= j){
+                    dp[i][j] = dp[i][j] || dp[i-1][j-num];
+                }
+            }
         }
 
-        if(index >= nums.length || target < 0){
-            return false;
-        }
-
-
-        if(dp[index][target] != null){
-            return dp[index][target];
-        }
-
-        //to include an element of nums arrya
-        boolean include = solve(nums, target - nums[index], dp, index+1);
-        //to exclude an element of nums array
-        boolean exclude = solve(nums, target, dp, index+1);
-
-        dp[index][target] = include || exclude;
-        return dp[index][target];
-
+        return dp[n][target];
     }
 }
 
