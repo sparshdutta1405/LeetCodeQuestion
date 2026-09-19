ca410bb86cf424e0e563ee31b21e6978a859ce5c
SPARSHDUTTA <195340215+sparshdutta1405@users.noreply.github.com>
2026-09-20 02:53:05 +0530

    Time: 0 ms (100%), Space: 45.6 MB (59.99%) - LeetHub - Time: O(n), Space: O(h)
    Balanced Binary Tree - LeetCode

---
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return checkHeight(root) != -1;
+    }
+
+    private int checkHeight(TreeNode node){
+        if(node == null)
+        return 0;
+
+        int leftHeight = checkHeight(node.left);
+        if(leftHeight == -1) return -1;
+
+        int rightHeight = checkHeight(node.right);
+        if(rightHeight == -1) return -1;
+
+        if(Math.abs(leftHeight - rightHeight) > 1)
+        return -1;
+
+        return 1 + Math.max(leftHeight, rightHeight);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
