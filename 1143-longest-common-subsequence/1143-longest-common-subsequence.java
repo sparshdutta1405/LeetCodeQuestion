25470e0d9a661d0ccb2d995e898c809390e5d58e
SPARSHDUTTA <195340215+sparshdutta1405@users.noreply.github.com>
2026-09-22 14:56:31 +0530

    Time: 25 ms (29.47%), Space: 54.1 MB (21.03%) - LeetHub - Time: O(n * m), Space: O(n * m)
    Longest common subsequence

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,36 +1,32 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-        int m = text1.length();
-        int n = text2.length();
+        int n = text1.length();
+        int m = text2.length();
 
+        int [][]dp=new int[n+1][m+1];
+        for(int [] rows:dp){
+            Arrays.fill(rows,-1);
+        }
         
-        if (n > m) {
-            
-            String tmp = text1; text1 = text2; text2 = tmp;
-            int t = m; m = n; n = t;
+        return solve(text1, text2, n - 1, m - 1,dp);
+    }
+
+    private int solve(String t1, String t2, int i, int j,int [][]dp) {
+        if (i < 0 || j < 0) {
+            return 0;
         }
+        
+        if(dp[i][j]!=-1) return dp[i][j];
 
-        int[] prev = new int[n + 1];
-        int[] curr = new int[n + 1];
-
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
-                    curr[j] = 1 + prev[j - 1];
-                } else {
-                    curr[j] = Math.max(prev[j], curr[j - 1]);
-                }
-            }
-            
-            int[] temp = prev;
-            prev = curr;
-            curr = temp;
-
+        if (t1.charAt(i) == t2.charAt(j)) {
+            return dp[i][j]=1 + solve(t1, t2, i - 1, j - 1,dp);
+        } 
+        else {
+            return dp[i][j]=Math.max(solve(t1, t2, i, j - 1,dp), solve(t1, t2, i - 1, j,dp));
         }
-        return prev[n];
     }
 }
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
