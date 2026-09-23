da03dc161496c8ae7369c366faebf57efdfdfce2
SPARSHDUTTA <195340215+sparshdutta1405@users.noreply.github.com>
2026-09-23 10:14:43 +0530

    Time: 20 ms (77.85%), Space: 53.8 MB (80.72%) - LeetHub - Time: O(m * n), Space: O(m * n)
    Longest Common Subsequence - LeetCode (Tabulation)

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,31 +1,25 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-        int n = text1.length();
-        int m = text2.length();
+        int m = text1.length();
+        int n = text2.length();
 
-        int [][]dp=new int[n+1][m+1];
-        for(int [] rows:dp){
-            Arrays.fill(rows,-1);
-        }
+        int[][] dp = new int[n + 1][m + 1];
+
         
-        return solve(text1, text2, n - 1, m - 1,dp);
-    }
 
-    private int solve(String t1, String t2, int i, int j,int [][]dp) {
-        if (i < 0 || j < 0) {
-            return 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (text2.charAt(i - 1) == text1.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
         }
-        
-        if(dp[i][j]!=-1) return dp[i][j];
 
-        if (t1.charAt(i) == t2.charAt(j)) {
-            return dp[i][j]=1 + solve(t1, t2, i - 1, j - 1,dp);
-        } 
-        else {
-            return dp[i][j]=Math.max(solve(t1, t2, i, j - 1,dp), solve(t1, t2, i - 1, j,dp));
-        }
+        return dp[n][m];
     }
-}
+}   
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
